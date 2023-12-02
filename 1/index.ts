de8c5194415f0b79f7439eba78fb1f390df2262c
Gregory ARTAUD <gregory.rtd@gmail.com>
2023-12-02 16:21:44 +0100

    fix: 1

--- 1/index.ts
+++ 1/index.ts
@@ -3,18 +3,24 @@ import { decryptLine as decryptLine_part2 } from "./part2";
 
 const getSum = (numbers: number[]) => {
   return numbers.reduce((acc, number) => acc + number, 0);
-}
+};
+
+let part1_sum = 0;
+let part2_sum = 0;
 
 for await (const chunk of Bun.stdin.stream()) {
   const chunkText = Buffer.from(chunk).toString();
-  const lines = chunkText.split('\n').filter((line) => line);
+  const lines = chunkText.split("\n").filter((line) => line);
 
   const part1_decryptedLines = lines.map(decryptLine_part1);
-  const part1_sum = getSum(part1_decryptedLines);
+  const part1_partial_sum = getSum(part1_decryptedLines);
 
   const part2_decryptedLines = lines.map(decryptLine_part2);
-  const part2_sum = getSum(part2_decryptedLines);
+  const part2_partial_sum = getSum(part2_decryptedLines);
 
-  console.log(`Part 1: ${part1_sum}`);
-  console.log(`Part 2: ${part2_sum}`);
+  part1_sum += part1_partial_sum;
+  part2_sum += part2_partial_sum;
 }
+
+console.log(`Part 1: ${part1_sum}`);
+console.log(`Part 2: ${part2_sum}`);
